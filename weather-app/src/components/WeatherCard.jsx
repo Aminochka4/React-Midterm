822ba52f74b23ac6dda86244ec8397e202cef853
Amina Amanzholova <eomino1607@gmail.com>
2025-10-24 12:59:18 +0500

    smth changed

--- weather-app/src/components/WeatherCard.jsx
+++ weather-app/src/components/WeatherCard.jsx
@@ -1,22 +1,17 @@
 import "./WeatherCard.css";
 
 function WeatherCard({ data, onMoreDetails }) {
-    const city = data.name;
-    const country = data.sys.country;
-    const temperature = Math.round(data.main.temp);
-    const description = data.weather[0].description;
-
     return (
         <div className="weather-card">
-            <p className="temp">{temperature}°C</p>
+            <p className="temp">{Math.round(data.main.temp)}°C</p>
 
             <h2 className="city">
-                {city}, <span className="country">{country}</span>
+                {data.name}, <span className="country">{data.sys.country}</span>
             </h2>
 
             <div className="description-block">
                 <p className="label">Description</p>
-                <p className="desc">{description}</p>
+                <p className="desc">{data.weather[0].description}</p>
             </div>
 
             <button className="details-btn" onClick={onMoreDetails}>
